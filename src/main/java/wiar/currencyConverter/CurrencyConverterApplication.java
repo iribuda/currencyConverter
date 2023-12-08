b73c07aa789ab078d94de534b75a3a8269174cc1
ayb23nee <aygerim.budaychieva.nee@fh-zwickau.de>
2023-12-08 17:32:20 +0100

    debugged

--- src/main/java/wiar/currencyConverter/CurrencyConverterApplication.java
+++ src/main/java/wiar/currencyConverter/CurrencyConverterApplication.java
@@ -5,9 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import wiar.currencyConverter.logic.Currency;
-
-import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
